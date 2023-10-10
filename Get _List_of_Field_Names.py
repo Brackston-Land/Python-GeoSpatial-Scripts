cc08457c8a0eeb935102cc025c1570c3813da83d
Brackston-Land <147285744+Brackston-Land@users.noreply.github.com>
2023-10-09 23:08:44 -0500

    Update Get _List_of_Field_Names.py

--- Get _List_of_Field_Names.py
+++ Get _List_of_Field_Names.py
@@ -1,10 +1,6 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
 # Author:      Brackston Land
 # Created:     13/01/2020
-# Copyright:   (c) Brackston Land 2020
-# Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
 import arcpy
