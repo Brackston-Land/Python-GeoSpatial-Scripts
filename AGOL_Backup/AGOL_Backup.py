1b240165caee8a3b128c4d24d4d9fe05512bbeb1
Brackston Land <147285744+Brackston-Land@users.noreply.github.com>
2023-10-10 09:44:58 -0500

    Update AGOL_Backup.py

--- AGOL_Backup/AGOL_Backup.py
+++ AGOL_Backup/AGOL_Backup.py
@@ -1,8 +1,6 @@
 ###############################################
+## code pulled from https://community.esri.com/t5/arcgis-survey123-questions/backup-survey-123-feature-layer/td-p/1041504
 ## Modified by Brackston Land 1/26/2022
-
-## https://community.esri.com/t5/arcgis-survey123-questions/backup-survey-123-feature-layer/td-p/1041504
-
 ## To use: open up the .txt that you create file and 
 ## on a new line add the output name of the what you want to download, a comma, and the Item ID of the item you want to backup. No spaces.
 ## Example ----->     Testing222,87858c26de2353255e4cdca32958a58gfg2370667e1
