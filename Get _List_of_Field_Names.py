55133a236218d1d86f81e552f05b4f51b0d20dd5
Brackston-Land <147285744+Brackston-Land@users.noreply.github.com>
2023-10-09 22:53:48 -0500

    Update Get _List_of_Field_Names.py

--- Get _List_of_Field_Names.py
+++ Get _List_of_Field_Names.py
@@ -1,16 +1,13 @@
 #-------------------------------------------------------------------------------
 # Name:        module1
 # Purpose:
-#
 # Author:      Brackston Land
-#
 # Created:     13/01/2020
 # Copyright:   (c) Brackston Land 2020
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
 import arcpy
-
 import json
 
 # For each field in the Hospitals feature class, print
@@ -21,4 +18,3 @@ for field in fields:
     print(field.name, field.type)
 
     #print (json.dumps(field.name) + ", ")
-
