d51d74b9f08049e7e806707cd65e1634aec91b9f
Blake Herrera <blakeherrera1@gmail.com>
2023-10-12 00:27:36 -0500

    Condense
    Condensed code by using standard library functions

--- Find_Number_of_Features_Of_Unique_Values_in_Field.py
+++ Find_Number_of_Features_Of_Unique_Values_in_Field.py
@@ -1,22 +1,23 @@
-#-------------------------------------------------------------------------------
-# Author:      Brackston Land
-# Created:     04/20/2020
-#-------------------------------------------------------------------------------
+'''
+The purpose of this script is to identify the number
+of features of unique values in arcpy fields.
 
+Authors:       Brackston Land, Blake Herrera
+Created:       04/20/2020
+'''
+
+# Standard libraries
+from collections import Counter
+from operator import itemgetter
+from pprint import pprint
+
+# Other libraries
 import arcpy
+
 
 fc = r"path to database connections"
 field = "wmConsumerType"
 
-#Create dictionary to store unique values
-CountDi = {}
-
-with arcpy.da.SearchCursor (fc, field) as cursor:
-    for row in cursor:
-        if not row[0] in CountDi.keys():
-            CountDi[row[0]] = 1
-        else:
-            CountDi[row[0]] += 1
-
-for key in CountDi.keys():
-    print str(key) + ":", CountDi[key], "features"
+with arcpy.da.SearchCursor(fc, field) as cursor:
+    # Count all of the first item in each row and format the print string
+    pprint(Counter(map(itemgetter(0), cursor)), width = 1)
